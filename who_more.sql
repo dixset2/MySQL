638269970fee6981fbecbc2cb69d63a95139de35
dixset2 <94751769+dixset2@users.noreply.github.com>
2021-12-22 23:30:48 +0300

    Delete who_more.sql

--- who_more.sql
+++
@@ -1,8 +0,0 @@
-SELECT IF(COUNT(women_profiles.user_id) > COUNT(men_profiles.user_id), 'women', 'men') AS who_more,
-       COUNT(men_profiles.user_id) AS men_likes_count,
-       COUNT(women_profiles.user_id) AS women_likes_count
-FROM likes LEFT JOIN profiles men_profiles on likes.user_id = men_profiles.user_id AND men_profiles.gender = 'm'
-           LEFT JOIN profiles women_profiles on likes.user_id = women_profiles.user_id AND women_profiles.gender = 'f';
-
-SELECT IF((SELECT COUNT(id) FROM likes WHERE user_id in (SELECT user_id FROM profiles WHERE gender = 'm')) >
-          (SELECT COUNT(id) FROM likes WHERE user_id in (SELECT user_id FROM profiles WHERE gender = 'f')), 'men', 'women') AS who_more;
